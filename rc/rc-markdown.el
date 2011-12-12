088e17d1d5988562595e2bb0faa9f6fd8d27427a
Kan-Ru Chen <kanru@kanru.info>
2011-12-12 10:36:16 +0800

    Autoload markdown mode

--- rc/rc-markdown.el
+++ rc/rc-markdown.el
@@ -1,5 +1,8 @@
 ;;; rc-markdown.el -- Markdown Mode Settings
 
+(autoload 'markdown-mode "~/src/mirror/markdown-mode/markdown-mode.el"
+  "Major mode for editing Markdown files" t)
+
 (add-to-list 'auto-mode-alist
              '("\\.mdwn\\'" . markdown-mode))
 
