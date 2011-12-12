92f5061433064307b53a08959e53c7875184b1bd
Kan-Ru Chen <kanru@kanru.info>
2011-12-12 10:47:48 +0800

    Start el-get transition

--- rc/rc-startup.el
+++ rc/rc-startup.el
@@ -3,4 +3,13 @@
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil t)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (end-of-buffer)
+    (eval-print-last-sexp)))
+(el-get 'sync)
+
 (provide 'rc-startup)
