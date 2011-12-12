ea5b4e67095d539ea53c5112489ddabe9d8674d7
Kan-Ru Chen <kanru@kanru.info>
2011-12-12 10:39:45 +0800

    Use dropdown list in yasnippet mode

--- rc/rc-yasnippet.el
+++ rc/rc-yasnippet.el
@@ -1,13 +1,19 @@
 ;;; rc-yasnippet.el -- Yasnippet Mode Settings
 
+(add-to-list 'load-path (expand-file-name "~/src/mirror/yasnippet"))
 (require 'yasnippet nil t)
 
 (eval-after-load 'yasnippet
-  '(progn
-     (setq yas/root-directory (list
-                               (locate-user-emacs-file "snippets")
-                               "/usr/share/emacs/site-lisp/yasnippet/snippets"))
-     (mapc 'yas/load-directory yas/root-directory)))
+  `(progn
+     (setq yas/root-directory '(,(locate-user-emacs-file "snippets")
+				"~/src/mirror/yasnippet/snippets")
+	   yas/prompt-functions '(yas/dropdown-prompt
+				  yas/x-prompt
+				  yas/completing-prompt
+				  yas/ido-prompt
+				  yas/no-prompt))
+     (mapc 'yas/load-directory yas/root-directory)
+     (yas/global-mode 1)))
 
 (provide 'rc-yasnippet)
 ;; rc-yasnippet.el ends here
