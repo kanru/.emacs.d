6736edc87feef3766f6dbf0ae4d03b8cb4ffcc54
Kan-Ru Chen <kanru@kanru.info>
2011-12-12 10:43:16 +0800

    Enable ido-anywhere mode

--- rc/rc-misc.el
+++ rc/rc-misc.el
@@ -28,6 +28,7 @@
 ;; ido-mode is like magic pixie dust!
 (when (> emacs-major-version 21)
   (ido-mode t)
+  (ido-everywhere 1)
   (setq ido-enable-prefix nil
         ido-enable-flex-matching t
         ido-create-new-buffer 'always
