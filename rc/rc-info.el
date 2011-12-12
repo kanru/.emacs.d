8668be336fe890864788f3e91ff2846b42326e8f
Kan-Ru Chen <kanru@kanru.info>
2011-12-12 10:35:10 +0800

    Add auto symbol lookup for lisp-mode

--- rc/rc-info.el
+++ rc/rc-info.el
@@ -2,4 +2,11 @@
 (add-to-list 'Info-default-directory-list "~/src/gnus/texi/")
 (add-to-list 'Info-default-directory-list "~/.emacs.d/info/")
 
+(require 'info-look)
+(info-lookup-add-help
+ :mode 'lisp-mode
+ :regexp "[^][()'\" \t\n]+"
+ :ignore-case t
+ :doc-spec '(("(ansicl)Symbol Index" nil nil nil)))
+
 (provide 'rc-info)
