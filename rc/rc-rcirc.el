c80f50b48c52937a1ef55b422f162d623d4cb716
Kan-Ru Chen <kanru@kanru.info>
2012-05-14 15:52:28 +0800

    Add advice for rcirc to read authinfo via auth-source API.

--- rc/rc-rcirc.el
+++ rc/rc-rcirc.el
@@ -44,5 +44,21 @@
       rcirc-keywords '("kanru"
                        "urnak"))
 
+(defadvice rcirc (around rcirc-read-from-authinfo activate)
+  "Allow rcirc to read authinfo from ~/.authinfo.gpg via the auth-source API."
+  (let ((rcirc-authinfo rcirc-authinfo)
+        (credentials (auth-source-search :port '("irc-nickserv")
+                                         :require '(:user :secret))))
+    (dolist (p credentials)
+      (let ((host (plist-get p :host))
+            (user (plist-get p :user))
+            (secret (plist-get p :secret)))
+        (add-to-list 'rcirc-authinfo
+                     (list host 'nickserv user
+                           (if (functionp secret)
+                               (funcall secret)
+                             secret)))))
+    ad-do-it))
+
 (provide 'rc-rcirc)
 ;;; rc-rcirc.el ends here
