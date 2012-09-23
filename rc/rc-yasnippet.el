da956b62c84b085e1be7a406431b347816ab4b7c
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2012-09-23 17:59:35 +0800

    Cleanup yasnippet settings

--- rc/rc-yasnippet.el
+++ rc/rc-yasnippet.el
@@ -4,16 +4,9 @@
 (require 'yasnippet nil t)
 
 (eval-after-load 'yasnippet
-  `(progn
-     (setq yas/root-directory `(,(locate-user-emacs-file "snippets")
-				"~/zone2/public/emacs/yasnippet/snippets")
-	   yas/prompt-functions '(yas/dropdown-prompt
-				  yas/x-prompt
-				  yas/completing-prompt
-				  yas/ido-prompt
-				  yas/no-prompt))
-     (mapc 'yas/load-directory yas/root-directory)
-     (yas/global-mode 1)))
+  '(progn
+     (setq yas-prompt-functions '(yas-ido-prompt))
+     (yas-global-mode 1)))
 
 (provide 'rc-yasnippet)
 ;; rc-yasnippet.el ends here
