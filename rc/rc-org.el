496519f31d25f504c2fe477d52bb681c72ec5600
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2012-12-04 09:49:42 +0800

    Re-org org-mode

--- rc/rc-org.el
+++ rc/rc-org.el
@@ -5,65 +5,40 @@
 
 (require 'org)
 (require 'org-checklist nil t)
-(require 'org-contacts nil t)
 
-(setq org-directory "~/zone1/org"
-      org-agenda-files '("~/zone1/org/newgtd.org")
-      diary-file "~/zone1/etc/diary"
-      ;org-agenda-include-diary t
-      calendar-mark-diary-entries-flag t
-      org-default-notes-file "journal.org"
-      org-log-done 'time
-      org-log-into-drawer "LOGBOOK"
-      org-clock-into-drawer "CLOCK"
-      ;org-clock-idle-time 30
-      org-refile-targets
-      '(("newgtd.org" :maxlevel . 1)
-        ("someday.org" :level . 2))
-      org-agenda-custom-commands
-      '(("p" . "Projects")
-        ("pp" "All Projects"
-         ((tags "PROJECT")))
-        ("ph" "Home Projects"
-         ((tags "PROJECT+HOME")))
-        ("po" "Office Projects"
-         ((tags "PROJECT+OFFICE")))
-        ("h" "Office and Home Lists"
-         ((agenda)
-          (tags-todo "OFFICE")
-          (tags-todo "HOME")
-          (tags-todo "DEBIAN")
-          (tags-todo "READING")))
-        ("d" "Daily Action List"
-         ((agenda "" ((org-agenda-ndays 1)
-                      (org-agenda-sorting-strategy
-                       '((agenda time-up priority-down tag-up)))
-                      (org-deadline-warning-days 0))))))
-      org-capture-templates
-      '(("u" "TODO: Read this URL" entry (file+datetree "reading.org")
-         "* UNREAD %(format-time-string \"%H:%M\") [[%^{URL}][%^{Description}]]"
-         :immediate-finish t)
-        ("r" "GTD: Reply this mail" entry (file+headline "newgtd.org" "Tasks")
-         "* TODO Reply mail from %:fromname %^g\n  %a")
-        ("t" "GTD: New task" entry (file+headline "newgtd.org" "Tasks")
-         "* TODO %^{Brief Description} %^g\n  %?")
-        ("p" "GTD: New project" entry (file+headline "newgtd.org" "Projects")
-         "* %^{Brief Description}")
-        ("i" "GTD: In-Basket" entry (file+headline "newgtd.org" "In-Basket")
-         "* %^{Brief Description}")
-        ("c" "Contacts" entry (file "contacts.org")
-         "* %(org-contacts-template-name)
-  :PROPERTIES:
-  :EMAIL:    %(org-contacts-template-email)
-  :END:"))
-      org-link-abbrev-alist
-      '(("debbugs"  . "http://bugs.debian.org/%s")
-        ("google"   . "https://www.google.com/search?q=")
-        ("duckgo"   . "https://duckduckgo.com/?q=")
-        ("gmap"     . "http://maps.google.com/maps?q=%s")
-        ("omap"     . "http://nominatim.openstreetmap.org/search?q=%s&polygon=1")))
+(setq diary-file "~/zone1/etc/diary"
+      calendar-mark-diary-entries-flag t)
 
-(setq org-contacts-files '("~/zone1/org/contacts.org"))
+(let ((quest-file (format-time-string "~/zone1/org/quests/%Y.org")))
+  (setq org-directory "~/zone1/org"
+        org-default-notes-file quest-file
+        org-agenda-files (list quest-file)
+        org-agenda-custom-commands
+        '(("d" "Daily Action List"
+           ((agenda "" ((org-agenda-ndays 1)
+                        (org-agenda-sorting-strategy
+                         '((agenda time-up priority-down tag-up)))
+                        (org-deadline-warning-days 0))))))
+        org-log-done 'time
+        org-log-into-drawer "LOGBOOK"
+        org-clock-into-drawer "CLOCK"
+        org-capture-templates
+        `(("r" "Reply this mail" plain
+           (file+olp ,quest-file "Quests" "Miscellaneous")
+           "   - [ ] Reply mail from %:fromname\n         %a")
+          ("n" "New misc quest" plain
+           (file+olp ,quest-file "Quests" "Miscellaneous")
+           "   - [ ] %?")
+          ("m" "New main quest" entry
+           (file+headline ,quest-file "Quests")
+           "** %?"))
+        org-link-abbrev-alist
+        '(("debbugs"  . "http://bugs.debian.org/%s")
+          ("google"   . "https://www.google.com/search?q=")
+          ("duckgo"   . "https://duckduckgo.com/?q=")
+          ("gmap"     . "http://maps.google.com/maps?q=%s")
+          ("omap"     . "http://nominatim.openstreetmap.org/search?q=%s&polygon=1")
+          ("bugzilla" . "http://bugzilla.mozilla.org/%s"))))
 
 (setq org-latex-to-pdf-process
       '("xelatex -interaction nonstopmode -output-directory %o %f"
