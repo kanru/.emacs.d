42a718ae43fbe8288a89143fbb3bb290223fc37f
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2013-07-09 22:33:09 +0800

    Install markdown-mode from ELPA.

--- rc/rc-markdown.el
+++ rc/rc-markdown.el
@@ -1,8 +1,5 @@
 ;;; rc-markdown.el -- Markdown Mode Settings
 
-(autoload 'markdown-mode "~/zone2/public/emacs/markdown-mode/markdown-mode.el"
-  "Major mode for editing Markdown files" t)
-
 (add-to-list 'auto-mode-alist
              '("\\.mdwn\\'" . markdown-mode))
 
