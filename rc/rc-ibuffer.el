ee66a3c7f5b4e7c83a7ed13aabf47319aa3b6889
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2013-07-15 12:10:09 +0800

    Update ibuffer.

--- rc/rc-ibuffer.el
+++ rc/rc-ibuffer.el
@@ -3,25 +3,24 @@
 (setq ibuffer-saved-filter-groups
       '(("default"
          ("mozilla" (or (filename . "mozilla")))
-         ("dired" (mode . dired-mode))
-         ("lisp" (or
-		  (mode . lisp-mode)
-		  (mode . emacs-lisp-mode)
-		  (name . "^\\*slime-")))
-         ("org" (or
-                 (mode . org-mode)))
-         ("gnus" (or
-                  (mode . message-mode)
-                  (mode . bbdb-mode)
-                  (mode . mail-mode)
-                  (mode . gnus-group-mode)
-                  (mode . gnus-summary-mode)
-                  (mode . gnus-article-mode)
-                  (name . "^\\.bbdb$")
-                  (name . "^\\.newsrc-dribble")
-                  (name . "^\\*gnus.*\\*$")))
-         ("irc" (or (mode . erc-mode)
-                    (mode . rcirc-mode))))))
+         ("mail/news"
+          (or (mode . message-mode)
+              (mode . bbdb-mode)
+              (mode . mail-mode)
+              (mode . rmail-mode)
+              (mode . rmail-summary-mode)
+              (mode . gnus-group-mode)
+              (mode . gnus-summary-mode)
+              (mode . gnus-article-mode)
+              (name . "^\\.newsrc-dribble")
+              (name . "^\\*gnus.*\\*$")))
+         ("lisp"
+          (or (mode . lisp-mode)
+              (mode . emacs-lisp-mode)
+              (name . "^\\*slime-")))
+         ("irc"
+          (or (mode . erc-mode)
+              (mode . rcirc-mode))))))
 (add-hook 'ibuffer-mode-hook
 	  (lambda ()
 	    (ibuffer-switch-to-saved-filter-groups "default")))
