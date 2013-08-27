4193f10a1c2a06c2070d62bcd1038164ced0f2ec
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2013-08-27 23:10:22 +0800

    Set gnus info directory

--- rc/rc-gnus.el
+++ rc/rc-gnus.el
@@ -1,5 +1,4 @@
 (add-to-list 'load-path (expand-file-name "~/zone2/public/emacs/gnus/lisp"))
-(add-to-list 'Info-default-directory-list "~/zone2/public/emacs/gnus/texi/")
 (require 'gnus-load nil t)
 
 (setq gnus-init-file (locate-user-emacs-file "gnus"))
