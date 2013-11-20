3ec40e7e67cfd408ca1ba9a3714ae4c3ad6d61e9
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2013-11-20 14:55:43 +0800

    Disable marmalade

--- rc/rc-packages.el
+++ rc/rc-packages.el
@@ -27,7 +27,7 @@
 (require 'package)
 (package-initialize)
 
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 
 (unless package-archive-contents
