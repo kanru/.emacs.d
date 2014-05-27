0eb2b9e3127ea3df20ab949568373efbef77127f
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2014-05-27 12:12:44 +0800

    Refined "mozilla" style for cc-mode

--- rc/rc-cc-mode.el
+++ rc/rc-cc-mode.el
@@ -25,87 +25,44 @@
 ;;; Code:
 
 (c-add-style "mozilla"
-	     '("k&r"
-	       (c-basic-offset . 2)	; Guessed value
-	       (c-offsets-alist
-		(access-label . -)
-		(arglist-cont . 0)	; Guessed value
-		(arglist-intro . +)
-		(block-close . 0)	; Guessed value
-		(class-close . 0)	; Guessed value
-		(class-open . 0)	; Guessed value
-		(defun-block-intro . +)	; Guessed value
-		(defun-close . 0)	; Guessed value
-		(defun-open . 0)	; Guessed value
-		(inclass . +)		; Guessed value
-		(inline-close . 0)	; Guessed value
-		(innamespace . 0)	; Guessed value
-		(member-init-cont . -)
-		(member-init-intro . +)
-		(namespace-close . 0)	; Guessed value
-		(statement . 0)		; Guessed value
-		(statement-block-intro . +) ; Guessed value
-		(statement-cont . +)	    ; Guessed value
-		(substatement . +)	; Guessed value
-		(topmost-intro . 0)	; Guessed value
-		(topmost-intro-cont . 0) ; Guessed value
-		(annotation-top-cont . 0)
-		(annotation-var-cont . +)
-		(arglist-close . c-lineup-close-paren)
-		(arglist-cont-nonempty . c-lineup-arglist)
-		(block-open . 0)
-		(brace-entry-open . 0)
-		(brace-list-close . 0)
-		(brace-list-entry . 0)
-		(brace-list-intro . +)
-		(brace-list-open . 0)
-		(c . c-lineup-C-comments)
-		(case-label . +)
-		(catch-clause . 0)
-		(comment-intro . c-lineup-comment)
-		(composition-close . 0)
-		(composition-open . 0)
-		(cpp-define-intro c-lineup-cpp-define +)
-		(cpp-macro . -1000)
-		(cpp-macro-cont . +)
-		(do-while-closure . 0)
-		(else-clause . 0)
-		(extern-lang-close . 0)
-		(extern-lang-open . 0)
-		(friend . 0)
-		(func-decl-cont . +)
-		(incomposition . +)
-		(inexpr-class . +)
-		(inexpr-statement . +)
-		(inextern-lang . +)
-		(inher-cont . c-lineup-multi-inher)
-		(inher-intro . +)
-		(inlambda . c-lineup-inexpr-block)
-		(inline-open . 0)
-		(inmodule . +)
-		(knr-argdecl . 0)
-		(knr-argdecl-intro . 0)
-		(label . 0)
-		(lambda-intro-cont . +)
-		(module-close . 0)
-		(module-open . 0)
-		(namespace-open . 0)
-		(objc-method-args-cont . c-lineup-ObjC-method-args)
-		(objc-method-call-cont c-lineup-ObjC-method-call-colons c-lineup-ObjC-method-call +)
-		(objc-method-intro .
-				   [0])
-		(statement-case-intro . +)
-		(statement-case-open . 0)
-		(stream-op . c-lineup-streamop)
-		(string . -1000)
-		(substatement-label . 0)
-		(substatement-open . 0)
-		(template-args-cont c-lineup-template-args +))))
+             '((indent-tabs-mode . nil)
+               (c-basic-offset . 2)
+               (c-comment-only-line-offset . 0)
+               (c-doc-comment-style . 'javadoc)
+               (c-hanging-braces-alist
+                (block-close)
+                (statement-case-open after)
+                (substatement-open after)
+                (class-open before after)
+                (class-close before)
+                (inline-open before after)
+                (inline-close before))
+               (c-hanging-colons-alist
+                (member-init-cont . before))
+               (c-offsets-alist
+                (inline-open . 0)
+                (innamespace . 0)
+                (label . -)
+                (case-label . +)
+                (substatement-label . -)
+                (substatement-open . 0))
+               (c-cleanup-list
+                (brace-else-brace
+                 brace-elseif-brace
+                 brace-catch-brace
+                 empty-defun-braces
+                 defun-close-semi
+                 list-close-comma
+                 scope-operator))))
+
+(set-default 'c-macro-names-with-semicolon
+             "\\_<NS_\\(IMPL\\|DECL\\|DEFINE\\|DECLARE\\)_.*\\_>")
 
 ;;; hide-ifdef-mode
 (setq hide-ifdef-shadow t
       hide-ifdef-initially t)
 (add-hook 'c-mode-common-hook 'hide-ifdef-mode)
+(add-hook 'c-mode-common-hook 'subword-mode)
 
 (provide 'rc-cc-mode)
 ;;; rc-cc-mode.el ends here
