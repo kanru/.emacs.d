6a55cf9ce70413c76bc8ad2a4b621f756fd40e0d
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2014-07-31 14:55:06 +0800

    Use js2-mode

--- rc/rc-js-mode.el
+++ rc/rc-js-mode.el
@@ -25,7 +25,8 @@
 ;;; Code:
 
 (setq js-indent-level 2)
-(add-to-list 'auto-mode-alist '("\\.jsm" . js-mode))
+(add-to-list 'auto-mode-alist '("\\.jsm" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js" . js2-mode))
 
 (provide 'rc-js-mode)
 ;;; rc-js-mode.el ends here
