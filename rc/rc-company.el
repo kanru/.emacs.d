975b0b7a6c6c5aae552fff9992dbf3bd9bb64dc2
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2014-08-29 18:28:03 +0800

    Add missing company config

---
+++ rc/rc-company.el
@@ -0,0 +1,32 @@
+;;; rc-company.el --- Company Mode                   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2014  Kan-Ru Chen (陳侃如)
+
+;; Author: Kan-Ru Chen (陳侃如) <kchen@mozilla.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(require 'company)
+
+(global-company-mode 1)
+
+(provide 'rc-company)
+;;; rc-company.el ends here
