fd6a27e17ab8b3b6bd612c0a57c38bd4f5776547
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2015-06-29 17:41:40 +0800

    Add magit config

---
+++ rc/rc-magit.el
@@ -0,0 +1,30 @@
+;;; rc-magit.el --- Magit                            -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  Kan-Ru Chen (陳侃如)
+
+;; Author: Kan-Ru Chen (陳侃如) <kanru@kanru.info>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(setq magit-last-seen-setup-instructions "1.4.0")
+
+(provide 'rc-magit)
+;;; rc-magit.el ends here
