0d88d17906eb73d4b84d2ebad4276d4b3340ddea
Kan-Ru Chen <kanru@kanru.info>
2017-07-31 13:58:50 +0800

    Use projectile mode to manage mozilla dir

--- rc/rc-ibuffer.el
+++ rc/rc-ibuffer.el
@@ -3,8 +3,7 @@
 (require 'ibuffer)
 
 (setq my-ibuffer-default-groups
-      `(("mozilla" (or (filename . ,(expand-file-name "~/mozilla"))))
-        ("journal" (filename . "journal/[0-9]+.txt$"))))
+      `(("journal" (filename . "journal/[0-9]+.txt$"))))
 (add-hook 'ibuffer-hook
 	  (lambda ()
             (setq ibuffer-saved-filter-groups
